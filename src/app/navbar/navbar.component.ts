9e348cd5f17fbd46b02ddbc36f9551442e71e015
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-13 16:48:56 +0100

    navbar content changes based on the selected language

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Language } from '../../shared-types';
+import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
+import { selectLanguage } from '../app.selectors';
+import { Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [LanguageSwitcherComponent, AsyncPipe],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  language$ = this.store.select(selectLanguage);
 
+  constructor(private store: Store) {}
 }
