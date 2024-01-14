800d5b030a69ffa267a4d9533c53c94f2b48a7ac
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-14 22:02:38 +0100

    completed logic and animations for the language switcher

--- src/app/language-switcher/language-switcher.component.ts
+++ src/app/language-switcher/language-switcher.component.ts
@@ -2,14 +2,62 @@ import { Component } from '@angular/core';
 import { Language } from '../../shared-types';
 import { Store } from '@ngrx/store';
 import { selectLanguage } from '../app.selectors';
-import { setLanguage } from '../app.actions';
+import { setLanguage, toggleLanguage } from '../app.actions';
+import { AsyncPipe, NgClass } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  stagger,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-language-switcher',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgClass],
   templateUrl: './language-switcher.component.html',
   styleUrl: './language-switcher.component.scss',
+  animations: [
+    trigger('toggleKnobAnimation', [
+      state(
+        'en',
+        style({
+          transform: 'translateX(-65%)',
+          background: 'url(../../assets/gb.svg)',
+          backgroundSize: 'contain',
+        })
+      ),
+      state(
+        'pl',
+        style({
+          transform: 'translateX(65%)',
+          background: 'url(../../assets/pl.svg)',
+          backgroundSize: 'contain',
+        })
+      ),
+      transition('en => pl', animate('0.2s')),
+      transition('pl => en', animate('0.2s')),
+    ]),
+    trigger('labelAnimation', [
+      state(
+        'hidden',
+        style({
+          opacity: 0,
+        })
+      ),
+      state(
+        'shown',
+        style({
+          opacity: 1,
+        })
+      ),
+      transition('hidden <=> shown', animate('0.2s')),
+    ]),
+  ],
 })
 export class LanguageSwitcherComponent {
   language$ = this.store.select(selectLanguage);
@@ -18,5 +66,9 @@ export class LanguageSwitcherComponent {
 
   changeLanguage(language: Language) {
     this.store.dispatch(setLanguage({ language }));
+  }
+
+  toggleLanguage() {
+    this.store.dispatch(toggleLanguage());
   }
 }
