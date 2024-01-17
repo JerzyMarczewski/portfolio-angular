05d17b1267e903f32d6c3406ef89f9e13ff5bd08
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-17 06:52:12 +0100

    completed about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,18 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { selectLanguage } from '../app.selectors';
+import { Store } from '@ngrx/store';
+import { TechnologiesButtonComponent } from '../technologies-button/technologies-button.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, TechnologiesButtonComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
+  language$ = this.store.select(selectLanguage);
 
+  constructor(private store: Store) {}
 }
