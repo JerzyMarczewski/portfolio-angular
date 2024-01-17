84865f659f2e72d9f680c070b787253f82fbd18d
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-17 20:28:06 +0100

    updated styles and structure

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectLanguage } from '../app.selectors';
+import { AsyncPipe } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      state(
+        'shown',
+        style({
+          opacity: 1,
+        })
+      ),
+      transition('* => shown', animate('1s ease-in')),
+    ]),
+  ],
 })
 export class HeroComponent {
+  language$ = this.store.select(selectLanguage);
+  heroTextShown: boolean = false;
 
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.heroTextShown = true;
+  }
 }
