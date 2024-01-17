aa5f6211e13940e3e8f5762d15b837fb26014a2a
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-17 21:48:57 +0100

    updated projects descriptions and styles

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { selectLanguage } from '../app.selectors';
+import { Store } from '@ngrx/store';
+import { ProjectComponent } from '../project/project.component';
+import { allTechologies } from '../technologies';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, ProjectComponent],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  language$ = this.store.select(selectLanguage);
 
+  asciiFilterTechnologies = allTechologies.filter((techology) =>
+    [
+      'React',
+      'Redux',
+      'TypeScript',
+      'JavaScript',
+      'HTML',
+      'CSS',
+      'Figma',
+    ].includes(techology.name)
+  );
+
+  mernRemindersTechnologies = allTechologies.filter((techology) =>
+    [
+      'React',
+      'Node.js',
+      'Express',
+      'MongoDB',
+      'Mongoose',
+      'JavaScript',
+      'HTML',
+      'CSS',
+      'Figma',
+    ].includes(techology.name)
+  );
+
+  viaggioTechnologies = allTechologies.filter((techology) =>
+    ['React', 'JavaScript', 'HTML', 'CSS', 'Figma'].includes(techology.name)
+  );
+
+  musicStreamerTechnologies = allTechologies.filter((techology) =>
+    [
+      'React',
+      'Redux',
+      'Supabase',
+      'TypeScript',
+      'JavaScript',
+      'HTML',
+      'CSS',
+    ].includes(techology.name)
+  );
+
+  movieSearcherTechnologies = allTechologies.filter((techology) =>
+    ['React', 'Supabase', 'TypeScript', 'JavaScript', 'HTML', 'CSS'].includes(
+      techology.name
+    )
+  );
+
+  constructor(private store: Store) {}
 }
