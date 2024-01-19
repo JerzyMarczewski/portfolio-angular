185a791d1bafb0f9021aeae583c3a48ba11fd3df
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-19 20:32:53 +0100

    created the technology interface and created the allTechnologies array

---
+++ src/app/technologies.ts
@@ -0,0 +1,99 @@
+export interface Technology {
+  name: string;
+  src: string;
+  url: string;
+}
+
+export const allTechologies: Technology[] = [
+  {
+    name: 'Angular',
+    src: '../../assets/technologies/angular.svg',
+    url: 'https://angular.io/',
+  },
+  {
+    name: 'React',
+    src: '../../assets/technologies/react.svg',
+    url: 'https://react.dev/',
+  },
+  {
+    name: 'NgRx',
+    src: '../../assets/technologies/ngrx.svg',
+    url: 'https://ngrx.io/',
+  },
+  {
+    name: 'Redux',
+    src: '../../assets/technologies/redux.svg',
+    url: 'https://redux.js.org/',
+  },
+  {
+    name: 'TypeScript',
+    src: '../../assets/technologies/typescript.svg',
+    url: 'https://www.typescriptlang.org/',
+  },
+  {
+    name: 'JavaScript',
+    src: '../../assets/technologies/javascript.svg',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/javascript',
+  },
+  {
+    name: 'HTML',
+    src: '../../assets/technologies/html.svg',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+  },
+  {
+    name: 'Sass',
+    src: '../../assets/technologies/scss.svg',
+    url: 'https://sass-lang.com/',
+  },
+  {
+    name: 'CSS',
+    src: '../../assets/technologies/css.svg',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+  },
+  {
+    name: 'MongoDB',
+    src: '../../assets/technologies/mongodb.svg',
+    url: 'https://www.mongodb.com/',
+  },
+  {
+    name: 'Mongoose',
+    src: '../../assets/technologies/mongoose.svg',
+    url: 'https://mongoosejs.com/',
+  },
+  {
+    name: 'Supabase',
+    src: '../../assets/technologies/supabase.svg',
+    url: 'https://supabase.com/',
+  },
+  {
+    name: 'MySQL',
+    src: '../../assets/technologies/mysql.svg',
+    url: 'https://www.mysql.com/',
+  },
+  {
+    name: 'Node.js',
+    src: '../../assets/technologies/nodejs.svg',
+    url: 'https://nodejs.org/en',
+  },
+
+  {
+    name: 'Express',
+    src: '../../assets/technologies/express.svg',
+    url: 'https://expressjs.com/',
+  },
+  {
+    name: 'Python',
+    src: '../../assets/technologies/python.svg',
+    url: 'https://www.python.org/',
+  },
+  {
+    name: 'Git',
+    src: '../../assets/technologies/git.svg',
+    url: 'https://git-scm.com/',
+  },
+  {
+    name: 'Figma',
+    src: '../../assets/technologies/figma.svg',
+    url: 'https://www.figma.com/',
+  },
+];
