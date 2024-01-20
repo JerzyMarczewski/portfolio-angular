fcf8d4c7aff942346536e5d4fd7fb874b53d9d22
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-20 15:55:01 +0100

    written tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,58 @@
 import { TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { AppComponent } from './app.component';
+import { setLanguage } from './app.actions';
 
 describe('AppComponent', () => {
+  let store: MockStore;
+  const initialState = {
+    language: 'en',
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideMockStore({ initialState })],
     }).compileComponents();
+
+    store = TestBed.inject(MockStore);
+
+    spyOn(store, 'dispatch').and.callThrough();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'portfolio-angular' title`, () => {
+  it('should set language to polish if browser uses polish', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-angular');
+
+    spyOnProperty(window.navigator, 'languages').and.returnValue([
+      'pl-PL',
+      'en-US',
+    ]);
+
+    app.ngOnInit();
+
+    expect(store.dispatch).toHaveBeenCalledWith(
+      setLanguage({ language: 'pl' })
+    );
   });
 
-  it('should render title', () => {
+  it('should set language to english if browser uses english', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio-angular');
+    const app = fixture.componentInstance;
+
+    spyOnProperty(window.navigator, 'languages').and.returnValue(['en-US']);
+
+    app.ngOnInit();
+
+    expect(store.dispatch).toHaveBeenCalledWith(
+      setLanguage({ language: 'en' })
+    );
   });
 });
