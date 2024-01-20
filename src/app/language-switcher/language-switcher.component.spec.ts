28358f672c906005e8faec9bb4785a22f2ec2afe
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-20 15:54:01 +0100

    written tests

--- src/app/language-switcher/language-switcher.component.spec.ts
+++ src/app/language-switcher/language-switcher.component.spec.ts
@@ -1,23 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { LanguageSwitcherComponent } from './language-switcher.component';
+import { toggleLanguage } from '../app.actions';
 
 describe('LanguageSwitcherComponent', () => {
   let component: LanguageSwitcherComponent;
+  let store: MockStore;
   let fixture: ComponentFixture<LanguageSwitcherComponent>;
+  const initialState = {
+    language: 'en',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LanguageSwitcherComponent]
-    })
-    .compileComponents();
-    
+      imports: [LanguageSwitcherComponent],
+      providers: [provideMockStore({ initialState }), provideAnimations()],
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(LanguageSwitcherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    spyOn(store, 'dispatch').and.callThrough();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch toggleLanguage action on handleToggleLanguage', () => {
+    component.handleToggleLanguage();
+
+    expect(store.dispatch).toHaveBeenCalledWith(toggleLanguage());
   });
 });
