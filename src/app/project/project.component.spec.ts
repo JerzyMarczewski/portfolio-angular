1a66a30c2a41605d187f30c6ce7b5fb5a3e16368
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2024-01-20 15:58:34 +0100

    written tests

--- src/app/project/project.component.spec.ts
+++ src/app/project/project.component.spec.ts
@@ -1,17 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { ProjectComponent } from './project.component';
 
 describe('ProjectComponent', () => {
   let component: ProjectComponent;
+  let store: MockStore;
   let fixture: ComponentFixture<ProjectComponent>;
+  const initialState = {
+    language: 'en',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProjectComponent]
-    })
-    .compileComponents();
-    
+      imports: [ProjectComponent],
+      providers: [provideMockStore({ initialState }), provideAnimations()],
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(ProjectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +26,16 @@ describe('ProjectComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should handle mouse over', () => {
+    component.handleMouseOver();
+    expect(component.isHovered).toBe(true);
+  });
+
+  it('should handle mouse leave', () => {
+    component.isHovered = true;
+    component.handleMouseLeave();
+    expect(component.isHovered).toBe(false);
   });
 });
